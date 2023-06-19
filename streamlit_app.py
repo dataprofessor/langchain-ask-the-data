0c7de22a9131856ce450bb2edf5c18a39755d2ce
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-19 13:39:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,17 @@ from langchain.agents import create_pandas_dataframe_agent
 st.set_page_config(page_title='🦜🔗 Ask the CSV App')
 st.title('🦜🔗 Ask the CSV App')
 
-def generate_response(input_csv, input_query):
-  llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
-  # Load CSV if file is uploaded
+# Load CSV file
+def load_csv(input_csv):
   df = pd.read_csv(input_csv)
-  # Display DataFrame
   with st.expander('See DataFrame'):
     st.write(df)
+  return df
+
+# Generate LLM response
+def generate_response(csv_file, input_query):
+  llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+  df = load_csv(csv_file)
   # Create Pandas DataFrame Agent
   agent = create_pandas_dataframe_agent(llm, df, verbose=True)
   # Perform Query using the Agent
@@ -23,13 +27,13 @@ def generate_response(input_csv, input_query):
 
 # File upload
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+
 # Query text
 # query_text = st.text_input('Enter your query:', placeholder = 'How many rows are there?', disabled=not uploaded_file)
 question_list = [
   'How many rows are there?',
-  'What is the range of values for MolWt with median solubility value greater than 0?',
-  'How many rows have solubility value greater than 0.'
-  ]
+  'What is the range of values for MolWt with median logS greater than 0?',
+  'How many rows have MolLogP value greater than 0.']
 query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
 
 openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
