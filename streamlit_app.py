85a0f9faca12968c40fed8224dfd0054db8aacba
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-20 20:22:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
 
 # Page title
-st.set_page_config(page_title='🦜🔗 Ask the Data App')
-st.title('🦜🔗 Ask the Data App')
+st.set_page_config(page_title='🦜🔗 Ask the Data app')
+st.title('🦜🔗 Ask the Data app')
 
 # Load CSV file
 def load_csv(input_csv):
