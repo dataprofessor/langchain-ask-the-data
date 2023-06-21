827213aa6a567e340bec16cfbb3f156d8c1a7bac
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-21 19:22:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,21 +25,19 @@ def generate_response(csv_file, input_query):
   response = agent.run(input_query)
   return st.success(response)
 
-# File upload
+# Input widgets
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
-
-# Query text
 question_list = [
   'How many rows are there?',
   'What is the range of values for MolWt with logS greater than 0?',
   'How many rows have MolLogP value greater than 0.',
   'Other']
 query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
-if query_text is 'Other':
-  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
 
 # App logic
-openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+if query_text is 'Other':
+  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
 if not openai_api_key.startswith('sk-'):
   st.warning('Please enter your OpenAI API key!', icon='⚠')
 if openai_api_key.startswith('sk-') and (uploaded_file is not None):
